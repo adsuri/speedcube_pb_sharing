2396e93bfbf5d4d511fee706597a6500e6c02cb3
Aditya Suri <aditya.as.suri@gmail.com>
2026-06-04 12:01:39 -0500

    Set up Cuber type and improve Puzzle and PBest types

---
+++ src/cuber/Cuber.ts
@@ -0,0 +1,32 @@
+import { Puzzle } from "../puzzle/Puzzle";
+import { PBest } from "../puzzle/PBest";
+
+export class Cuber {
+  id: number = 0;
+  name: string = "";
+  email: string = "";
+  pictureUrl: string = "";
+  puzzles: Record<string, PBest | null> = {};
+
+  constructor(initializer?: any) {
+    if (!initializer) throw new Error("Provide a Cuber initializer...");
+    if (initializer.id) this.id = initializer.id;
+    if (initializer.name) this.name = initializer.name;
+    if (initializer.email) this.email = initializer.email;
+    if (initializer.pictureUrl) this.pictureUrl = initializer.pictureUrl;
+
+    let valid_puzzles: string[] = [
+      "222", "333", "444", "555", "666", "777",
+      "3bld", "4bld", "5bld", "mbld", "skewb", "squan",
+      "pyra", "mega", "fmc", "clock", "oh"
+    ];
+
+    for (const puzzle of valid_puzzles) {
+      if (puzzle in initializer.puzzles) {
+        this.puzzles[puzzle] = initializer.puzzles[puzzle];
+      } else {
+        this.puzzles[puzzle] = null;
+      }
+    }
+  }
+}
--- src/puzzle/Puzzle.ts
+++ src/puzzle/Puzzle.ts
@@ -1,39 +1,24 @@
 import { PBest } from "./PBest"
 
 export class Puzzle {
-  id: string;
-  currMain: string | undefined;
-  single: PBest | undefined;
-  mo3: PBest | undefined;
-  ao5: PBest | undefined;
-  ao12: PBest | undefined;
-  ao25: PBest | undefined;
-  ao50: PBest | undefined;
-  ao100: PBest | undefined;
+  currMain: string = "";
+  bests: Record<string, PBest | null> = {};
 
   constructor(initializer?: any) {
-    let valid_ids: string[] = [
-      "222", "333", "444", "555", "666", "777",
-      "3bld", "4bld", "5bld", "mbld", "skewb", "squan",
-      "pyra", "mega", "fmc", "clock", "oh"
-    ];
-
     if (!initializer) throw new Error("Provide a Puzzle initializer...");
-    if (!initializer.id) throw new Error("Provide a Puzzle id...")
-
-    if (initializer.id in valid_ids) {
-      this.id = initializer.id;
-    } else {
-      throw new Error("Provide a valid id for a Puzzle...")
-    }
-
     if (initializer.currMain) this.currMain = initializer.currMain;
-    if (initializer.single) this.single = initializer.single;
-    if (initializer.mo3) this.mo3 = initializer.mo3;
-    if (initializer.ao5) this.ao5 = initializer.ao5;
-    if (initializer.ao12) this.ao12 = initializer.ao12;
-    if (initializer.ao25) this.ao25 = initializer.ao25;
-    if (initializer.ao50) this.ao50 = initializer.ao50;
-    if (initializer.ao100) this.ao100 = initializer.ao100;
+    if (!initializer.bests) throw new Error("Provide scores for a Puzzle...");
+
+    let categories: string[] = [
+      "single", "mo3", "ao5", "ao12", "ao25", "ao50", "ao100"
+    ]
+
+    for (const category of categories) {
+      if (category in initializer.bests) {
+        this.bests[category] = initializer.bests[category];
+      } else {
+        this.bests[category] = null;
+      }
+    }
   }
 }
